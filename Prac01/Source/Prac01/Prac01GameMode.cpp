7a78ceda4ab1881935318ae51ce031061e857873
Lacty <akira206@gmail.com>
2016-10-16 19:10:40 +0900

    add Prac01
    default levelを設定した

---
+++ Prac01/Source/Prac01/Prac01GameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Prac01.h"
+#include "Prac01GameMode.h"
+
+
+
+
---
+++ Prac01/Source/Prac01/Prac01GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Prac01GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRAC01_API APrac01GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
