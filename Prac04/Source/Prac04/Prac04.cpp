b335f2a250b241b1140935df1357d2fd9b8b37a2
Lacty <akira206@gmail.com>
2016-10-19 13:04:44 +0900

    add Prac04
    HUD test

---
+++ Prac04/Source/Prac04/Prac04.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Prac04.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Prac04, "Prac04" );
---
+++ Prac04/Source/Prac04/Prac04GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Prac04GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRAC04_API APrac04GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
